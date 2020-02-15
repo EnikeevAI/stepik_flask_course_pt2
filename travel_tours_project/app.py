0bbf78ad1cf144d8b94b4c1519607754fbb0cc63
eai <alexander.enikeev@gmail.com>
2020-02-15 18:57:25 +0300

    add templates for rout's

--- travel_tours_project/app.py
+++ travel_tours_project/app.py
@@ -1,19 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from data import title, subtitle, description, departures, tours
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'main page'
+    return render_template('index.html', title=title, subtitle=subtitle, description=description)
 
 @app.route('/departure/<departure>')
 def departure(departure):
-    return 'departure page'
+    return render_template('departure.html')
 
 @app.route('/tour/<tour_id>')
 def tour(tour_id=1):
-    return 'tour page'
+    return render_template('tour.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
